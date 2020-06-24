5e0378bd1aa42c8cda4f103393415eec8338a79f
LCN29 <CanLee529@gmail.com>
2020-06-24 15:19:40 +0800

    remove the wrong hint

--- common-utils/src/main/java/com/lcn29/kit/util/OSUtil.java
+++ common-utils/src/main/java/com/lcn29/kit/util/OSUtil.java
@@ -18,19 +18,19 @@ public class OSUtil {
 
     private final static String OS_NAME = "os.name";
 
-    public final static String getOSName() {
+    public static String getOSName() {
         return System.getProperty(OS_NAME);
     }
 
-    public final static boolean isWindow() {
+    public static boolean isWindow() {
         return getOSName().toLowerCase().contains(WINDOW);
     }
 
-    public final static boolean isLinux() {
+    public static boolean isLinux() {
         return getOSName().toLowerCase().contains(LINUX);
     }
 
-    public final static boolean isMac() {
+    public static boolean isMac() {
         return getOSName().toLowerCase().contains(MAC);
     }
 
